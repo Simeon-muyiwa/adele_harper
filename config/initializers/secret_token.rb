50dfc99831ca77255a8b98500d48ef385ed69478
Simeon Muyiwa <muyiwa100@yahoo.com>
2014-07-19 17:30:04 +0100

    Adds .gitignore file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AdeleHarper::Application.config.secret_key_base = '4cac69e3f0eb0977bd1837530ab19ac3e03d2121b47c44644a4a715fbd1615dfb88b335735d3d1563599ae615035a63db17fa787de13dbaefc3be2220865ea2d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AdeleHarper::Application.config.session_store :cookie_store, key: '_adele_harper_session'
